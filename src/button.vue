ae961f88afeb5a18c83700559415588a5138944c
suxun <871728578@qq.com>
2019-08-10 20:56:56 +0800

    button.vue

--- src/button.vue
+++ src/button.vue
@@ -1,47 +1,97 @@
 <template>
-  <button class="g-button" :class="iconPosition">
-  <!-- <button class="g-button" :class="{right:iconPosition}"> -->
-    <svg v-if="icon"  class="icon" :class="test">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
-    <slot></slot>
+  <button class="g-button" :class="`icon-${iconPosition}`" @click="onClick">
+    <g-icon v-if="icon && !loading" :name="icon" class="icon">
+    </g-icon>
+    <g-icon name="loading" v-if="loading"  class="loading icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 
 <script>
 export default {
   name: "",
-  props:{
-    icon:String,
-    iconPosition:{
-      type:String,
-      // default:'left'
+  props: {
+    loading:{
+      type:Boolean,
+      default:false
     },
-    test:{
-      type:Array,
-      default:[]
+    icon: String,
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value){
+        return value === 'left' || value === "right"
+      }
     }
-  }
+  },
+  methods: {
+    onClick(){
+      this.$emit('click')
+    }
+  },
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100%{
+    transform: rotate(360deg)
+  }
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
   border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  // border: 1px solid var(--border-color);
+  // background: var(--button-bg);
+  border: 1px solid transparent;
+  transition: all 0.3s cubic-bezier(0.645, 0.45, 0.355, 1);
+  color: rgba(0, 0, 0, 0.65);
+  background-color: #fff;
+  border-color: #d9d9d9;
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.015);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
-    border-color: var(--border-color-hover);
+    // border-color: var(--border-color-hover);
+    color: #40a9ff;
+    border-color: #40a9ff;
   }
   &:active {
-    background-color: var(--button-active-bg);
+    // background-color: var(--button-active-bg);
+    color: #096dd9;
+    border-color: #096dd9;
   }
 
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin: 0 0 0 0.3em;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+  .loading{
+    animation: spin 2s infinite linear
   }
 }
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props:['name']
+};
+</script>
+
+<style lang="less" scoped>
+.g-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.05em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
