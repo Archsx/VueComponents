1c316ddce9e939068667e56c87dc83c0651c9af5
suxun <871728578@qq.com>
2019-08-24 15:05:57 +0800

    custom layout

---
+++ components/layout/Sider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Sider'
+}
+</script>
+
+<style lang="less" scoped>
+  .sider{
+    position: relative;
+  }
+</style>
---
+++ components/layout/app.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import Layout from './Layout.vue'
+import Header from './Header.vue'
+import Sider from './Sider.vue'
+import Content from './Content.vue'
+import Footer from './Footer.vue'
+
+
+
+
+Vue.component('g-layout',Layout)
+Vue.component('g-header',Header)
+Vue.component('g-sider',Sider)
+Vue.component('g-content',Content)
+Vue.component('g-footer',Footer)
+
+
+new Vue({
+  el:'#app',
+  data() {
+    return {
+      siderStyle:{
+        width:'200px'
+      }
+    }
+  },
+  methods: {
+    toggleHide(){
+      this.siderStyle.width = this.siderStyle.width === '200px'? '40px':'200px'
+    }
+  },
+})
